09762764b318abe0c4d00e5c7a50435224fda985
salman8896 <salmanshahid8896@gmail.com>
2023-08-06 18:34:06 +0500

    Ping Pong Game using Socketio

---
+++ server.js
@@ -0,0 +1,37 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
+
+console.log(`listening on port ${PORT}`);
+let readyPlayedrCount = 0;
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  socket.on("ready", () => {
+    console.log("Player Ready", socket.id);
+
+    readyPlayedrCount++;
+
+    if (readyPlayedrCount % 2 == 0) {
+      io.emit("startGame", socket.id);
+    }
+  });
+
+  socket.on("paddleMove", (paddleData) => {
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
+  socket.on("disconnect", (reason) => {
+    console.log(`Client ${socket.id} disconnected. Reason: ${reason}.`);
+  });
+});
